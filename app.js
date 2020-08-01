a89b242a5a5ffb03959c056244c540e618e434b1
Shubham <shubhamnagpal30@gmail.com>
2020-08-01 20:37:55 +0530

    Add About, Contact and compose pages and routes

--- app.js
+++ app.js
@@ -18,8 +18,25 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
-app.get('/', function(req, res) {
-  res.render('home', {startingContent: homeStartingContent});
+app.get('/', function (req, res) {
+  res.render('home', { startingContent: homeStartingContent });
+});
+
+app.get('/about', function (req, res) {
+  res.render('about', { about: aboutContent });
+});
+
+app.get('/contact', function (req, res) {
+  res.render('contact', { contact: contactContent });
+});
+
+app.get('/compose', function (req, res) {
+  res.render('compose');
+});
+
+app.post('/compose', function(req, res) {
+  console.log(req.body.postTitle);
+  res.redirect('/compose');
 })
 
 app.listen(3000, function () {
