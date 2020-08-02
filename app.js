9fff886811bb6ec9b2f7ae7e6009c91275d52c86
Shubham <shubhamnagpal30@gmail.com>
2020-08-02 19:25:26 +0530

    Compare params using lodash

--- app.js
+++ app.js
@@ -1,8 +1,7 @@
-//jshint esversion:6
-
 const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
+const _ = require('lodash');
 
 const homeStartingContent =
   'Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.';
@@ -23,6 +22,18 @@ let posts = [];
 app.get('/', function (req, res) {
   res.render('home', { startingContent: homeStartingContent, allPosts: posts });
 });
+
+app.get('/posts/:postName', function(req, res) {
+  const requestedTitle = _.lowerCase(req.params.postName);
+  posts.forEach(function(post) {
+    const storedTitle = _.lowerCase(post.title);
+    if(storedTitle === requestedTitle) {
+      console.log('Match Found');
+    } else {
+      console.log();
+    }
+  })
+})
 
 app.get('/about', function (req, res) {
   res.render('about', { about: aboutContent });
